fc00eba29913a9f5e9577bfb22d77d253de9e013
Matthew <mchen146@ucsc.edu>
2022-10-08 16:30:08 -0700

    static bugs

--- bug.js
+++ bug.js
@@ -3,18 +3,19 @@
 
 class Bug extends WorldObject{
 
+    static bugs = [];
+
     constructor(options = {}){
         super(options)
         this.spawnlineY = 20;
         this.lastSpawn = -1;
-        this.bugs = [];
         this.bug = {
             x:Math.random()*(canvas.width-30)+15,
             y:this.spawnlineY
         }
     }
 
-    spawnBug(){
+    static spawnBug(){
         this.bugs.push(this.bug);
     }
 
