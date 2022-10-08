a7be0e169b00661f5bf96dda8fc957531f09efc6
Matthew <mchen146@ucsc.edu>
2022-10-08 15:38:20 -0700

    added world

---
+++ world.js
@@ -0,0 +1,28 @@
+
+let canvasWidth = 100;
+let canvasHeight = 100;
+let cameraPos = vec(0,0);
+
+function getCanvasPos(pos) {
+
+}
+
+function getWorldPos(pos) {
+
+}
+
+
+class WorldObject {
+    constructor(options = {}) {
+        this.pos = options.pos || vec(0,0);
+        this.char = options.char;
+    }
+
+    update() {
+
+    }
+
+    draw() {
+
+    }
+}
