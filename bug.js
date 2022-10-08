ab9d9774390d3b915a664690989e533527417fce
Nicholas Lau <nklau@ucsc.edu>
2022-10-08 16:12:31 -0700

    bug.js

---
+++ bug.js
@@ -0,0 +1,23 @@
+
+
+
+class Bug extends WorldObject{
+
+    constructor(options = {}){
+        super(options)
+        this.spawnlineY = 20;
+        this.lastSpawn = -1;
+        this.bugs = [];
+        this.bug = {
+            x:Math.random()*(canvas.width-30)+15,
+            y:this.spawnlineY
+        }
+    }
+
+
+    update(){
+        
+    }
+
+    
+}
