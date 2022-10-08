9996fe286d2342e3384c979176383fe4353e7779
Matthew <mchen146@ucsc.edu>
2022-10-08 16:56:27 -0700

    workingon frog

--- frog.js
+++ frog.js
@@ -1,13 +1,56 @@
 
 
 class Frog extends WorldObject {
+
+    static states = {
+        FALLING: 0,
+        FIRING: 1,
+        SWINGING: 2,
+        RELOADING: 3,
+    }
+
     constructor(options = {}) {
         super(options);
+        this.state = Frog.states.FALLING
+
+        this.tongueTipPos = vec(this.pos);
+        this.isTongueTipVisible = false;
     }
 
     update() {
         
+        switch (this.state) {
+            case Frog.states.FALLING:
+                if (input.isPressed) {
+                    this.state = Frog.states.FIRING;
+                    this.isTongueTipVisible = true;
+                }
+                break;
+            case Frog.states.FIRING:
+                this.isTongueTipVisible = true;
+                if (input.isPressed) {
+
+                }
+                break;
+            case Frog.states.SWINGING:
+            
+                break;
+            case Frog.states.RELOADING:
+            
+                break;
+            default:
+                break;
+        }
+
+
     }
 
-    
+    draw(canvasPos) {
+
+        if (this.isTongueTipVisible) {
+            box(getCanvasPos(this.tongueTipPos), vec(3,3));
+        }
+
+        super.draw(canvasPos);
+    }
 }
--- main.js
+++ main.js
@@ -7,18 +7,22 @@ characters = [];
 
 options = {};
 
+let player;
+
 function update() {
     if (!ticks) {
   
         WorldObject.reset();
         cameraPos = vec(0,0);
-        new WorldObject({
-            box: vec(3,3),
-            color: 'red',
-            gravityScale: .5
-        }).update = function() {
-            // this.pos.x = Math.sin(ticks * .1) * 20;
-        };
+        // new WorldObject({
+        //     box: vec(3,3),
+        //     color: 'red',
+        //     gravityScale: .5
+        // }).update = function() {
+        //     // this.pos.x = Math.sin(ticks * .1) * 20;
+        // };
+
+        player = new Frog({box: vec(6,6), color: 'green'});
     }
 
     // Bug.spawnBug()
